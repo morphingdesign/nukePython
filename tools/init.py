8b025e2e542ea6c87aefd9c95ce813004832fc81
Hans Palacios <architect.hp@gmail.com>
2020-11-02 17:14:44 -0600

    Added required code snippet for Cryptomatte plugin. Path updated to include Cryptomatte clone directory.

--- tools/init.py
+++ tools/init.py
@@ -1,4 +1,14 @@
+# NUKE_PATH
 # NUKE_PATH env var set to target directory where this init.py is located.
-nuke.pluginAddPath('./NukeSurvivalToolkit/NukeSurvivalToolkit/')
 nuke.pluginAddPath('./Gizmos/')
 nuke.pluginAddPath('./Icons/')
+# Nuke Survival Toolkit cloned from GitHub, path identified for source.
+nuke.pluginAddPath('./NukeSurvivalToolkit/NukeSurvivalToolkit/')
+# Cryptomatte Plugin cloned from GitHub, path identified for source.
+nuke.pluginAddPath('./Cryptomatte/nuke')
+
+# Required for Cryptomatte Plugin; Path defined above via NUKE_Path.
+# Ref docs for more info about plugin.
+# Source: https://github.com/Psyop/Cryptomatte
+import cryptomatte_utilities
+cryptomatte_utilities.setup_cryptomatte()
