a7d9766220f9fb3ee091e848df746509b2595845
Hans Palacios <architect.hp@gmail.com>
2021-04-05 14:07:31 -0500

    Updated for use with Nuke 13.0v1. Cryptomatte plugin removed from customization since it is now part of default Nuke 13. Nuances with Python 3 updated in all applicable plugin scripts.

--- tools/init.py
+++ tools/init.py
@@ -4,13 +4,5 @@ nuke.pluginAddPath('./Gizmos/')
 nuke.pluginAddPath('./Icons/')
 # Nuke Survival Toolkit cloned from GitHub, path identified for source.
 nuke.pluginAddPath('./NukeSurvivalToolkit/NukeSurvivalToolkit/')
-# Cryptomatte Plugin cloned from GitHub, path identified for source.
-nuke.pluginAddPath('./Cryptomatte/nuke')
 # PixelFudger Plugin accessed via Nukepedia, path identified for source.
 nuke.pluginAddPath('./Gizmos/PixelFudger/')
-
-# Required for Cryptomatte Plugin; Path defined above via NUKE_Path.
-# Ref docs for more info about plugin.
-# Source: https://github.com/Psyop/Cryptomatte
-import cryptomatte_utilities
-cryptomatte_utilities.setup_cryptomatte()
